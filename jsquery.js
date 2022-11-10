5aa463813982f07e221d97c51b685d057b901d03
akshay dits <akshaykumarwins@gmail.com>
2022-11-10 16:54:59 +0530

    try

---
+++ jsquery.js
@@ -0,0 +1,289 @@
+
+var x = 0;
+$(document).ready(function(){
+
+  //click function   
+  /*$("p").click(function(){
+    $(this).hide();
+  });*/
+
+//   dbclick
+  /*$("p").dblclick(function(){
+    $(this).hide();
+  });*/  
+
+//   mouseenter
+ /* $("p").mouseenter(function(){
+    alert("wooooow!");
+  });*/
+
+// mouseleave
+/* $("#base").mouseleave(function(){
+    alert("Bye! You leave me alone base!");
+  });*/
+
+//   mousedwon
+  /*$(".ball").mousedown(function(){
+    alert("Mouse down over ball!");
+  });*/
+
+//   mouseup
+  /*$(".ball").mouseup(function(){
+    alert("Mouse up over base!");
+  });*/
+// console
+  /*$("#base").mousedown(function(){
+    console.log("Mouse down over p1!");
+  });*/
+  /*$("#base").mouseup(function(){
+    console.log("Mouse up over p1!");
+  });*/
+
+// hover
+ /* $("#base").hover(function(){
+    alert("You entered world!");
+  },
+  function(){
+    alert("Bye! You now leave world!");
+  });*/ 
+
+
+// focus blur 
+ /* $("input").focus(function(){
+    $(this).css("background-color", "yellow");
+  });
+  
+  $("input").blur(function(){
+    $(this).css("background-color", "red");
+  });*/
+
+// on
+ /* $("p").on("click", function(){
+    $(this).hide();
+  });*/ 
+
+//   sumit
+//   $("form").submit(function(){
+//     alert("Submit world");
+//   });
+//   $("#sum").click(function(){
+//     $("form").submit();
+//   });  
+
+// sumit by input tag mathod
+// $("form").submit(function(){
+//     alert("Submitted");
+//   });
+
+// resize
+// $(window).resize(function(){
+//     $("span").text(x += 1);
+//   }); 
+
+
+// resize with button
+// $(window).resize(function(){
+//     $("span").text( x+= 1);
+//   });
+//   $(".size").click(function(){
+//     $(window).resize();
+//   });
+
+// scroll
+// $("#advant").scroll(function(){
+//     $("span").text( x+= 1);
+//   });
+//   $(".scol").click(function(){
+//     $("div").scroll();
+//   });
+
+// change the text pop
+// $("input").change(function(){
+//     alert("The text has been changed.");
+//   });
+
+// onhange show input text
+// $("#sum").click(function(){
+//     $("#inval").change();
+//   });
+
+
+
+
+  // $("p").on("mousedown", function(){
+  //   console.log("hiii");
+  // });
+  
+  // hide timely
+  // $("button").click(function(){
+  //   $("p").hide(2000);
+  // });
+
+  // hide show without time
+  // $(".hide").click(function(){
+  //   $("p").hide();
+  // });
+
+  // $(".show").click(function(){
+  //   $("p").show();
+  // });
+
+  // toggle
+  // $("#click").click(function(){
+  //   // $("p , h1" ).toggle();
+  //   $("*" ).toggle();
+  // });
+
+// fade in
+ 
+// $(".fadein").click(function(){
+//     $(".p").fadeIn();
+//     $(".sun").fadeIn("slow");
+//     $(".tree").fadeIn(3000);
+//   });
+
+//   fadeout
+//   $(".fadeout").click(function(){
+//         $(".p").fadeOut();
+//         $(".sun").fadeOut("slow");
+//         $(".tree").fadeOut(3000);
+//       });
+
+
+// fadetoggle
+// $(".fadetoggle").click(function(){
+//     $(".p1").fadeToggle();
+//     $(".p2").fadeToggle("slow");
+//     $(".p3").fadeToggle(3000);
+//   });
+
+// fadeto
+// $(".fadeto").click(function(){
+//     $(".dit").fadeTo("slow", 0.3);
+//     $(".dits").fadeTo("slow", 0.4);
+//     $(".ditstek").fadeTo("slow", 0.7);
+//   });
+
+// slideDown
+// $("#flip").click(function(){
+//     $("#panel").slideDown("slow");
+//   });
+
+// slideup
+// $("#side").click(function(){
+//     $("#docs").slideUp("slow");
+//   });
+
+// slidetoggle
+// $("#flip").click(function(){
+//     $("#panel").slideToggle("slow");
+//   });
+
+// annimate left
+// $("#anni").click(function(){
+//     $("#glow").animate({left: '250px'});
+//   });
+
+
+// get 
+// $("#btn1").click(function(){
+//     alert("Text: " + $("#test").text());
+//   });
+//   $("#btn2").click(function(){
+//     alert("HTML: " + $("#test").html());
+//   });
+//   $("#btn3").click(function(){
+//    console.log("Value:    " + $("#txt").val());
+//   });
+
+// set
+// $("#btn1").click(function(){
+//     $("#test1").text("Hello life");
+//   });
+//   $("#btn2").click(function(){
+//     $("#test2").html("<b>Hello world!</b>");
+//   });
+//   $("#btn3").click(function(){
+//     $("#txt").val("america");
+//   });
+
+
+// set new and old
+// $("#btn1").click(function(){
+//     $("#test1").text(function(i, origText){
+//       return "Old text: " + origText + " New text: Hello lifw (index: " + i + ")"; 
+//     });
+//   });
+
+//   $("#btn2").click(function(){
+//     $("#test2").html(function(i, origText){
+//       return "Old html: " + origText + " New html: Hello <b>wow life!</b> (index: " + i + ")"; 
+//     });
+//   });
+
+// append
+// $("#app").click(function(){
+//     $(".z").append(" <b>Appended text</b>.");
+//   });
+
+//   $("#lit").click(function(){
+//     $("ol").append("<li>Appended item</li>");
+//   });
+
+// prepend
+// $("#app").click(function(){
+//     $(".z").prepend(" <b>prepend text</b>.");
+//   });
+
+//   $("#lit").click(function(){
+//     $("ol").prepend("<li>prepend item</li>");
+//   });
+
+// after before
+// $("#bt1").click(function(){
+//     $("img").before("<b>hello dits</b>");
+//   });
+
+//   $("#bt2").click(function(){
+//     $("img").after("<i>by by dits</i>");
+//   });
+
+// remove
+// $("#re").click(function(){
+//     $("p").remove(".test");
+//   });
+
+
+// empty
+// $("#emp").click(function(){
+//     $("p").empty(".test");
+//   });
+
+// addclass
+// $("#clbtn").click(function(){
+//     $("#test , .test").addClass("blue impt");
+//   });
+
+// remove class
+// $("#reclbtn").click(function(){
+//     $("p")  .removeClass("blue imp");
+//   });
+
+// toggleClass
+// $("#togbtn").click(function(){
+//     $("p").toggleClass("blue");
+//   });
+
+
+// css
+// $("#btncss").click(function(){
+//     $("p").css({"background-color": "yellow", "font-size": "200%"});
+//   });
+
+
+// $("#").click(function(){
+//     $("#w3s").attr("href", "https://www.w3schools.com/jquery/");
+//   });
+
+  
+});
